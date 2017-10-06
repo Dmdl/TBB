f801537b3b12788f4600dc967e249fa3fcf4a026
lakmal <lada@mazarin.lk>
2017-10-06 14:32:37 +0530

    changed

--- index.js
+++ index.js
@@ -6,51 +6,119 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
+// var bot = new builder.UniversalBot(connector, function (session) {
+//     session.send("Hi... Welcome to the ABC Bank Bot.");
+//     session.send("We will help you with banking needs. What would you like to do?");
+//     var msg = new builder.Message(session);
+//     msg.attachmentLayout(builder.AttachmentLayout.carousel);
+//     msg.attachments([
+//         new builder.HeroCard(session)
+//             .title("online bank")
+//             .text("all online banking")
+//             .buttons([
+//                 builder.CardAction.imBack(session, "online Bank", "onlineBank")
+//             ]),
+//         new builder.HeroCard(session)
+//             .title("product info")
+//             .text("product information")
+//             .buttons([
+//                 builder.CardAction.imBack(session, "productInformation", "Proceed")
+//             ]),
+//         new builder.HeroCard(session)
+//             .title("promotions")
+//             .text("promotions")
+//             .buttons([
+//                 builder.CardAction.imBack(session, "promotions", "Proceed")
+//             ]),
+//         new builder.HeroCard(session)
+//             .title("Branches & ATM")
+//             .text("Branches & ATM")
+//             .buttons([
+//                 builder.CardAction.imBack(session, "Branches & ATM", "Proceed")
+//             ]),
+//         new builder.HeroCard(session)
+//             .title("Rates and Tariffs")
+//             .text("rates and tariffs")
+//             .buttons([
+//                 builder.CardAction.imBack(session, "Rates and Tariffs", "Proceed")
+//             ])
+//     ]);
+//     session.send(msg);
+// });
+
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("Hi... Welcome to the ABC Bank Bot.");
-    session.send("We will help you with banking needs. What would you like to do?");
-    var msg = new builder.Message(session);
-    msg.attachmentLayout(builder.AttachmentLayout.carousel);
-    msg.attachments([
-        new builder.HeroCard(session)
-            .title("online bank")
-            .text("all online banking")
-            .buttons([
-                builder.CardAction.imBack(session, "onlineBank", "onlineBank")
-            ]),
-        new builder.HeroCard(session)
-            .title("product info")
-            .text("product information")
-            .buttons([
-                builder.CardAction.imBack(session, "productInformation", "Proceed")
-            ]),
-        new builder.HeroCard(session)
-            .title("promotions")
-            .text("promotions")
-            .buttons([
-                builder.CardAction.imBack(session, "promotions", "Proceed")
-            ]),
-        new builder.HeroCard(session)
-            .title("Branches & ATM")
-            .text("Branches & ATM")
-            .buttons([
-                builder.CardAction.imBack(session, "Branches & ATM", "Proceed")
-            ]),
-        new builder.HeroCard(session)
-            .title("Rates and Tariffs")
-            .text("rates and tariffs")
-            .buttons([
-                builder.CardAction.imBack(session, "Rates and Tariffs", "Proceed")
-            ])
-    ]);
-    session.send(msg);
+    session.send("Hi... We sell shirts. Say 'show shirts' to see our products.");
 });
 
-bot.dialog('onlineBank', [
-    function (session) {
-        session.send('online banking').endDialog();
+// Add dialog to return list of shirts available
+bot.dialog('showShirts', function (session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+            .title("Classic White T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+            ]),
+        new builder.HeroCard(session)
+            .title("Classic Gray T-Shirt")
+            .subtitle("100% Soft and Luxurious Cotton")
+            .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+            .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+            .buttons([
+                builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+            ])
+    ]);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: /^(show|list)/i });
+
+// Add dialog to handle 'Buy' button click
+bot.dialog('buyButtonClick', [
+    function (session, args, next) {
+        // Get color and optional size from users utterance
+        var utterance = args.intent.matched[0];
+        var color = /(white|gray)/i.exec(utterance);
+        var size = /\b(Extra Large|Large|Medium|Small)\b/i.exec(utterance);
+        if (color) {
+            // Initialize cart item
+            var item = session.dialogData.item = {
+                product: "classic " + color[0].toLowerCase() + " t-shirt",
+                size: size ? size[0].toLowerCase() : null,
+                price: 25.0,
+                qty: 1
+            };
+            if (!item.size) {
+                // Prompt for size
+                builder.Prompts.choice(session, "What size would you like?", "Small|Medium|Large|Extra Large");
+            } else {
+                //Skip to next waterfall step
+                next();
+            }
+        } else {
+            // Invalid product
+            session.send("I'm sorry... That product wasn't found.").endDialog();
+        }
     },
-]).triggerAction({ matches: /(onlineBank|bank)\s.*bank/i });
+    function (session, results) {
+        // Save size if prompted
+        var item = session.dialogData.item;
+        if (results.response) {
+            item.size = results.response.entity.toLowerCase();
+        }
+
+        // Add to cart
+        if (!session.userData.cart) {
+            session.userData.cart = [];
+        }
+        session.userData.cart.push(item);
+
+        // Send confirmation to users
+        session.send("A '%(size)s %(product)s' has been added to your cart.", item).endDialog();
+    }
+]).triggerAction({ matches: /(buy|add)\s.*shirt/i });
 
 var server = restify.createServer();
 server.post('/api/messages', connector.listen());
